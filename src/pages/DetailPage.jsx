9e9ced9f1ee1dea3909f1ed0c5d976a467d208e7
administrator <administrator@administratornoMacBook-Air.local>
2026-03-15 22:33:14 +0900

    課題2の実装

---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useParams, Link} from 'react-router-dom';
+import posts from "../data/posts";
+
+export default function DetailPage() {
+    const { id } = useParams();
+    const post = posts.find((p) => p.id === Number(id));
+
+    return (
+        <article>
+            <img src={post.thumbnailUrl} alt={post.title} className="w-full h-64 object-cover mb-8" />
+            <div className="flex justify-between items-center mb-8">
+                <span className="text-gray-400">{post.createdAt.slice(0, 10)}</span>
+                <div className="flex gap-2">
+                    {post.categories.map(cat => <span key={cat} className="bg-black text-white px-2 py-1 text xs">{cat}</span>)}
+                </div>
+            </div>
+            <h1 className="text-3xl font-bold mb-8">{post.title}</h1>
+            <div className="leading-loose" dangerouslySetInnerHTML={{ __html: post.content}} />
+        </article>
+    );
+}
